84f6508ca8bec6c2ddaaa82ba27d43783d8d5555
ariel zarate <arieltecnico@gmail.com>
2026-03-23 12:22:46 -0300

    add new project

---
+++ Holamundo-Thymeleaf-Mvc/src/main/java/com/arielzarate/Holamundo_Thymeleaf_Mvc/HolamundoThymeleafMvcApplication.java
@@ -0,0 +1,13 @@
+package com.arielzarate.Holamundo_Thymeleaf_Mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolamundoThymeleafMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolamundoThymeleafMvcApplication.class, args);
+	}
+
+}
---
+++ Holamundo-Thymeleaf-Mvc/src/test/java/com/arielzarate/Holamundo_Thymeleaf_Mvc/HolamundoThymeleafMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arielzarate.Holamundo_Thymeleaf_Mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolamundoThymeleafMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
